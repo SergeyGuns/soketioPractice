c294d8018a81e30ad010c92143a9495817c308b4
Sergey <sg@onpoint.ru>
2016-10-13 23:20:39 +0300

    fix

---
+++ index.js
@@ -0,0 +1,59 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('public/index.html');
+});
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('прилетело на сервер:',msg)
+    io.emit('chat message', msg);
+  });
+  socket.on('key down A' , ()=>{
+    manipulationObj.x -=1
+    console.log(manipulationObj.x)
+    // io.emit('key down A', manipulationObj);
+  })
+  socket.on('key up A' , ()=>{
+    manipulationObj.x -=1
+    console.log(manipulationObj.x)
+    // io.emit('key down A', manipulationObj);
+  })
+  socket.on('key press S' , ()=>{
+    manipulationObj.y +=1
+    console.log(manipulationObj.y)
+    io.emit('key press A', manipulationObj);
+  })
+  socket.on('key press D' , ()=>{
+    manipulationObj.x +=1
+    console.log(manipulationObj.x)
+    io.emit('key press A', manipulationObj);
+  })
+  socket.on('key press W' , ()=>{
+    manipulationObj.y -=1
+    console.log(manipulationObj.y)
+    io.emit('key press A', manipulationObj);
+  })
+  socket.on('mousemove' , (mouse)=>{
+    console.log(mouse)
+    io.emit('mousemove', mouse);
+  })
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+let manipulationObj = {
+  x:0,
+  y:0
+}
